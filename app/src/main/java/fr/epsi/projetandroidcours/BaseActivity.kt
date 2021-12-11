407d5fc9a26f90172bd3ba3658a3cf2158a15e47
Baratin.pro <baratin.pro@gmail.com>
2021-12-11 11:26:19 +0100

    PDE-3: Add header

--- app/src/main/java/fr/epsi/projetandroidcours/BaseActivity.kt
+++ app/src/main/java/fr/epsi/projetandroidcours/BaseActivity.kt
@@ -1,6 +1,12 @@
 package fr.epsi.projetandroidcours
 
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 open class BaseActivity: AppCompatActivity() {
+
+    fun setHeaderTitle(text:String){
+        val textViewTitle = findViewById<TextView>(R.id.textViewTitle)
+        textViewTitle.text=text
+    }
 }
--- app/src/main/java/fr/epsi/projetandroidcours/MainActivity.kt
+++ app/src/main/java/fr/epsi/projetandroidcours/MainActivity.kt
@@ -1,11 +1,11 @@
 package fr.epsi.projetandroidcours
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setHeaderTitle("EPSI")
     }
 }
